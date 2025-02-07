2d5dfdbc5136dd709c845278680e0d5076a77345
Tunde Alabi <tundealabi4780@gmail.com>
2025-02-07 04:15:40 +0100

    feat: add readme; add basic imfo endpoint;

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # be-stage-0
+
 HNG Stage 0 Backend - Develop a Public API to Retrieve Basic Information
+
+## Getting Started
+
+To get started with the repository, follow these steps:
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/tundealabi/be-stage-0.git
+```
+
+2. Change into the project directory:
+
+```bash
+cd be-stage-0
+```
+
+3. Start the Node JS server:
+
+```bash
+npm start | yarn start
+```
+
+## 📜 Endpoint
+
+## `/` - Base Path
+
+This endpoint returns basic information in a structured format as a JSON response.
+
+- URL: http://localhost:4300
+- Method: GET
+- Status Code: 200 (OK)
+- Response:
+
+```json
+{
+  "email": "your-email@example.com",
+  "current_datetime": "2025-01-30T09:30:00Z",
+  "github_url": "<https://github.com/yourusername/your-repo>"
+}
+```
+
+## 💻 Built with
+
+Technologies used in the project:
+
+- Javascript/Node Js - <https://hng.tech/hire/nodejs-developers>
---
+++ server.mjs
@@ -0,0 +1,38 @@
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, OPTIONS'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    res.statusCode = 204;
+    res.end();
+    return;
+  }
+
+  // Handle other requests
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.end(
+      JSON.stringify({
+        email: 'tundealabi4780@gmail.com',
+        current_datetime: new Date().toISOString().split('.').at(0) + 'Z',
+        github_url: 'https://github.com/tundealabi/be-stage-0',
+      })
+    );
+  } else {
+    res.statusCode = 404;
+    res.end(JSON.stringify({ error: 'Not Found' }));
+  }
+});
+
+const PORT = 4300;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
